4c1921264986d5fdee3b120dac39198d98429504
cloudrohu <141917927+cloudrohu@users.noreply.github.com>
2025-08-29 12:51:48 +0530

    Window

--- alamdarsystemwindow/asgi.py
+++ alamdarsystemwindow/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for imaaratdesign project.
+ASGI config for alamdarsystemwindow project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'imaaratdesign.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'alamdarsystemwindow.settings')
 
 application = get_asgi_application()
--- alamdarsystemwindow/wsgi.py
+++ alamdarsystemwindow/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for imaaratdesign project.
+WSGI config for alamdarsystemwindow project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'imaaratdesign.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'alamdarsystemwindow.settings')
 
 application = get_wsgi_application()
